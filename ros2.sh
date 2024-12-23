e121fe7723b3d1c6301897b44a8287512409050c
NBT03 <caycuk72@gmail.com>
2024-12-23 13:45:42 +0700

    mode: real

---
+++ ros2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Thiết lập môi trường ROS2 (thay bằng đường dẫn ROS2 của bạn nếu khác)
+source /opt/ros/humble/setup.bash
+
+# Thiết lập workspace ROS2 (nếu có workspace riêng)
+# source ~/ros2_ws/install/setup.bash
+
+# Chạy lệnh launch
+ros2 launch dsr_bringup2 dsr_bringup2_gazebo.launch.py mode:=real host:=192.168.137.100 model:=m0609
